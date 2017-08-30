089ef7cfc3e58d8d291a8ece87ac53ce04bfee14
Jhonatan Giraldo <jhonatangiraldo111@gmail.com>
2017-08-29 22:35:50 -0500

    Se conecta registro con back

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { LoginService } from './../services/login.service';
 import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
@@ -12,7 +14,8 @@ export class RegisterComponent implements OnInit {
   public username: string;
   public password: string;
   public confirmPassword: string;
-  constructor() { }
+  constructor(private loginService: LoginService,
+              private router: Router) { }
 
   ngOnInit() {
     this.differentPass = false;
@@ -26,7 +29,23 @@ export class RegisterComponent implements OnInit {
       this.differentPass = true;
     } else {
       this.differentPass = false;
+      this.register();
     }
+  }
+
+  /**
+   * register in bd
+   */
+  private register() {
+    this.loginService.register(this.username, this.password).subscribe((response: Response) => {
+        let data: any = response.json();
+        if (data.success === true) {
+            alert("User registered");
+            this.router.navigate(['/login']);
+        } else {
+            alert("Server response with error");
+        }
+    });
   }
 
 }
